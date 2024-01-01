7ad15d5e75f2284803fe05bf98e47df4a8b5e0fd
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2024-01-01 13:09:36 +0100

    Finish day 24 in prolog

--- 24.pl
+++ 24.pl
@@ -1,4 +1,4 @@
-:- use_module(library(clpq)).
+:- use_module(library(clpq)), use_module(library(clpfd)).
 
 main :-
     read_string(user_input, _, S), split_string(S, "\n", "\n", Lines),
@@ -8,7 +8,7 @@ main :-
     ), Lines, Stones),
 
     aggregate_all(count, (
-        maplist(slice(2), Stones, Stones2D),
+        maplist([St,R]>>append(R, [_], St), Stones, Stones2D),
         { T1 >= 0, T2 >= 0 },
         append(L, [A|_], Stones2D), member(B, L),
         maplist({T1,T2}/[P1-V1, P2-V2]>>{
@@ -18,6 +18,18 @@ main :-
             % 7 =< P, P =< 27
         }, A, B)
     ), Part1),
-    format("Part 1: ~d~n", Part1).
+    format("Part 1: ~d~n", Part1),
 
-slice(N, L, S) :- length(S, N), append(S, _, L).
+    Params = [X-XV, Y-YV, Z-ZV],
+    [XV,YV,ZV] ins -3..300,
+    X #>= 0, Y #>= 0, Z #>= 0,
+    maplist({Params}/[St, T]>>(
+        T #> 0,
+        maplist({T}/[P1-V1, P2-V2]>>(
+            P1 #= P2 + (V2 - V1) * T
+            % P1 + V1 * T #= P2 + V2 * T
+        ), St, Params)
+    ), Stones, Times),
+    once(label([X,Y,Z,XV,YV,ZV|Times])),
+    Part2 is X+Y+Z,
+    format("Part 2: ~d~n", Part2).
