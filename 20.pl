788d5abd3d98c514d01bff36f9013bc1484e44b2
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2024-01-09 22:10:13 +0100

    Finish AoC in Prolog

--- 20.pl
+++ 20.pl
@@ -6,9 +6,9 @@ str(S) --> string(Cs), { string_codes(S, Cs) }.
 module(mod(Name, K, Adj)) -->
     ([K], { memberchk(K, `%&`) }; {K = 0'.}), str(Name), " -> ", sequence(str, ", ", Adj), "\n".
 
-:- chr_constraint counter/2, pulse/1.
+:- chr_constraint counter/2, pulse/2.
 
-counter(P, C), pulse(P) <=> NC is C+1, counter(P, NC).
+counter(P, C), pulse(P, _) <=> NC is C+1, counter(P, NC).
 
 main :-
     phrase_from_stream(sequence(module, Mods), user_input),
@@ -22,16 +22,22 @@ main :-
         ht_put(State, Name, conj(Prev))
     ), Mods, _), !,
 
+    (part1(State), fail; true),
+    part2(State).
+
+part1(State) :-
     counter(0, 0), counter(1, 0),
-    foreach((between(1, 1000, _), Q = [pulse("button", 0, "broadcaster")|Tail]),
-        process(Q-Tail, State)),
+    foreach(between(1, 1000, _), press(State)),
     findall(C, current_chr_constraint(counter(_, C)), [L, H]),
     Part1 is L * H,
-    format("Part 1: ~d~n", [Part1]).
+    format("Part 1: ~d~n", [Part1]), !.
 
-process([]-_, _).
-process([pulse(From, P, To)|Rest]-QT, State) :-
-    pulse(P),
+press(State) :-
+    Q = [pulse("button", 0, "broadcaster")|Tail],
+    foldl(process(State), Q, Tail, []).
+
+process(State, pulse(From, P, To), QT, NQT) :-
+    pulse(P, From),
     (ht_get(State, To, Spec) ->
         (Spec = fflop(PS) ->
             (P =:= 1 -> NQT = QT;
@@ -39,12 +45,25 @@ process([pulse(From, P, To)|Rest]-QT, State) :-
          Spec = conj(Lows),
             (P =:= 1 -> subtract(Lows, [From], NLows); union(Lows, [From], NLows)),
             (NLows == [] -> NP is 0; NP is 1),
-            ht_put(State, To, conj(NLows)),
-            send(To, NP, QT, NQT));
-        send(To, P, QT, NQT)),
-    process(Rest-NQT, State).
+            ht_put(State, To, conj(NLows)), send(To, NP, QT, NQT));
+        send(To, P, QT, NQT)).
 
 send(From, V, QT, NQT) :-
-    adj(From, L) ->
-        foldl({From,V}/[To, [pulse(From, V, To)|Tail], Tail]>>true, L, QT, NQT);
-        QT = NQT.
+    findall(pulse(From, V, To), (adj(From, L), member(To, L)), QT, NQT).
+
+:- chr_constraint presses/1, first_high/2.
+
+presses(X), pulse(_, "button") <=> Y is X+1, presses(Y).
+first_high(X, Y), presses(C) \ pulse(1, X) <=> var(Y) | Y = C.
+pulse(_, _) <=> true.
+
+part2(State) :-
+    presses(0),
+    findall(P, (adj(PP, ["rx"]), adj(P, Adj), memberchk(PP, Adj)), Prev),
+    maplist([P, H]>>first_high(P, H), Prev, Hs),
+    solve(State, Hs).
+
+solve(State, Hs) :- maplist(number, Hs) ->
+    foldl([H, A, R]>>(R is H * A), Hs, 1, Part2),
+    format("Part 2: ~d~n", [Part2]);
+    press(State), solve(State, Hs).
